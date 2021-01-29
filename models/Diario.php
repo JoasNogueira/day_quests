462718c0b5b0719c28685c24d5006c2b8f834065
Joas Nogueira <joas.caem@gmail.com>
2021-01-29 16:15:35 -0300

    Adicionando model diario

---
+++ models/Diario.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace models;
+
+class Quests
+{
+
+    public  $id;
+    public  $id_usuario;
+    public  $titulo;
+    public  $descricao;
+    public  $diarioData;
+
+
+
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_usuario
+     */
+    public function getId_usuario()
+    {
+        return $this->id_usuario;
+    }
+
+    /**
+     * Set the value of id_usuario
+     *
+     * @return  self
+     */
+    public function setId_usuario($id_usuario)
+    {
+        $this->id_usuario = $id_usuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of titulo
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     *
+     * @return  self
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descricao
+     */
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * Set the value of descricao
+     *
+     * @return  self
+     */
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of diarioData
+     */
+    public function getDiarioData()
+    {
+        return $this->diarioData;
+    }
+
+    /**
+     * Set the value of diarioData
+     *
+     * @return  self
+     */
+    public function setDiarioData($diarioData)
+    {
+        $this->diarioData = $diarioData;
+
+        return $this;
+    }
+};
